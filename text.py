a7d127e9d30b8dc8a578a9f3992a9a837933cf88
Alex J <imalexj@ust.hk>
2026-06-23 12:25:18 +0800

    Week 2

--- text.py
+++ text.py
@@ -1,1 +1,11 @@
 print("I enrolled in the Python for Everyone course!")
+
+tot = 0 
+for i in [5, 4, 3, 2, 1] :
+    tot = tot + 1
+print(tot)
+
+zork = 0
+for thing in [9, 41, 12, 3, 74, 15] :
+    zork = zork + thing
+print('After', zork)
